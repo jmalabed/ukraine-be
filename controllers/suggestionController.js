5417eeeb5f869e675bdd6e3195ce15cca317a121
jmalabed <jmalabed27@gmail.com>
2022-03-28 14:23:23 -0700

    updated suggestion controller

--- controllers/suggestionController.js
+++ controllers/suggestionController.js
@@ -54,6 +54,8 @@ router.delete("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
+    const prevSuggestions = await Suggestion.get({});
+    console.log(prevSuggestions);
     const newSuggestion = await Suggestion.create(req.body);
     res.status(200).json(newSuggestion);
   } catch (e) {
